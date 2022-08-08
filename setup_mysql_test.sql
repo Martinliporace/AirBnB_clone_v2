4b8a3f1c71a7f5d9a4f466cc58c2d56ed4ca615f
Martinliporace <98000916+Martinliporace@users.noreply.github.com>
2022-08-08 16:13:16 -0300

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,3 +2,4 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'hbnb_test_pwd'@'localhost'
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
