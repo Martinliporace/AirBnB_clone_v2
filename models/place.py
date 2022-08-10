aab55c4b0d6752a85dc96bd0fb38e2f46f0a5b49
root <martin.liporace@gmail.com>
2022-08-10 16:20:17 -0700

    place.py

--- models/place.py
+++ models/place.py
@@ -30,24 +30,13 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        amenity_ids = []
+
 
         reviews = relationship("Review", backref="place")
         amenities = relationship("Amenity", secondary="place_amenity",
                                  backref="place_amenities", viewonly=False)
 
     else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
 
         @property
         def reviews(self):
