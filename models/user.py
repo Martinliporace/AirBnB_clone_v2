3b5c326a60e119dbaa84531587fad096a7406e16
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-08-10 13:51:40 -0700

    Fixed user.py

--- models/user.py
+++ models/user.py
@@ -7,8 +7,15 @@ from sqlalchemy import Column, String
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    if (models.type_storage == "db"):
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
