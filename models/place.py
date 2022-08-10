9a02d3f0faa4476967ba01b3d2101d294a03da81
root <martin.liporace@gmail.com>
2022-08-10 15:30:53 -0700

    place.py

--- models/place.py
+++ models/place.py
@@ -6,16 +6,6 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 import models
 
-metadata = MetaData()
-if (models.type_storage == "db"):
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey("places.id"),
-                                 primary_key=True, nullable=False),
-                          Column('amenity_id', String(60),
-                                 ForeignKey("amenities.id"),
-                                 primary_key=True, nullable=False))
-
 
 class Place(BaseModel, Base):
     """ A place to stay """
