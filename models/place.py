0828bd5c6f35fe3976d80960c382f66d54c41532
root <martin.liporace@gmail.com>
2022-08-10 16:47:56 -0700

    place.py

--- models/place.py
+++ models/place.py
@@ -60,12 +60,12 @@ class Place(BaseModel, Base):
             if type(amenities) == Amenity:
                 new_list = []
                 self.new_list.append(amenities.id)
-
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60),
-                             ForeignKey("places.id"),
-                             primary_key=True, nullable=False),
-                      Column('amenity_id', String(60),
-                             ForeignKey("amenities.id"),
-                             primary_key=True, nullable=False))
+if (models.type_storage == "db"):
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
 
