6b71f311cbdf1e585e3719ecbcf5cfd9fa2d7e44
root <martin.liporace@gmail.com>
2022-08-10 15:50:17 -0700

    place.py

--- models/place.py
+++ models/place.py
@@ -19,18 +19,19 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
 
-    city_id = Column(String(60),  ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-
     if (models.type_storage == "db"):
+        city_id = Column(String(60),  ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        amenity_ids = []
+
         reviews = relationship("Review", backref="place")
         amenities = relationship("Amenity", secondary="place_amenity",
                                  backref="place_amenities", viewonly=False)
@@ -66,3 +67,10 @@ class Place(BaseModel, Base):
             amenity_ids that contains all Amenity.id linked to the Place """
 
             return self.amenities
+
+        @amenities.setter
+        def amenities_setter(self, amenities=None):
+            """setter of amenities"""
+            if type(amenities) == Amenity:
+                am_list = []
+                self.am_ist.append(amenities.id)
