9b4d6b5258cb5ae56f94be69090f488e67c21044
root <martin.liporace@gmail.com>
2022-08-10 17:26:48 -0700

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 import models
-from models.place import place_amenity
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -11,4 +10,3 @@ class Amenity(BaseModel, Base):
     """ inherits from BaseModel and Base """
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
