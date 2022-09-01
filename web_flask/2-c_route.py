c9b91f8624809595bd08e416b01f4899dd2b8e88
martinliporace <martin.liporace@gmail.com>
2022-09-01 13:54:13 +0000

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+"""starts a Flask web application"""
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_route():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hello_route2():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def hello_route3(text):
+    text2 = 'C {}'.format(text.replace('_', ' '))
+    return text2
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
