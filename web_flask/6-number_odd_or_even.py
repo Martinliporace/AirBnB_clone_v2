961d222086cfcf4fe2bfe8d7544f646b271f2c3b
martinliporace <martin.liporace@gmail.com>
2022-09-01 21:54:19 +0000

    6-number_odd_or_even.py

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+"""starts a Flask web application"""
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_route():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hello_route2():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def hello_route3(text):
+    text2 = 'C {}'.format(text.replace('_', ' '))
+    return text2
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def hello_route4(text='is cool'):
+    text2 = 'Python {}'.format(text.replace('_', ' '))
+    return text2
+
+
+@app.route("/number/<n>")
+def hello_route5(n):
+    text = int(n)
+    text2 = '{} is a number'.format(n)
+    return text2
+
+
+@app.route("/number_template/<n>")
+def hello_route6(n):
+    num = int(n)
+    if type(num) is int:
+        return render_template('5-number.html', num=n)
+
+
+@app.route("/number_odd_or_even/<n>")
+def hello_route7(n):
+    num = int(n)
+    typeN = ''
+    if (num % 2) == 0:
+        typeN = 'even'
+    else:
+        typeN = 'odd'
+    if type(num) is int:
+        return render_template('6-number_odd_or_even.html', num=n, typeN=typeN)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
