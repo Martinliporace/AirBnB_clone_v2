a38b645ec1b729c7ece3bb420129bb30384b06a7
martinliporace <martin.liporace@gmail.com>
2022-09-01 13:25:04 +0000

    0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+"""starts a Flask web application"""
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_route():
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
