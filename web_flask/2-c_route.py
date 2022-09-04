e880a66ddc2458df7d5fc48ec31071e4cc01d24e
martinliporace <martin.liporace@gmail.com>
2022-09-04 12:49:22 +0000

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""task 2"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,16 +9,19 @@ app.url_map.strict_slashes = False
 
 @app.route("/")
 def hello_route():
+    """display Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb")
 def hello_route2():
+    """HBNB"""
     return "HBNB"
 
 
 @app.route("/c/<text>")
 def hello_route3(text):
+    """ display “C ” followed by the value of the text variable"""
     text2 = 'C {}'.format(text.replace('_', ' '))
     return text2
 
