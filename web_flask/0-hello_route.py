b6050a3317ec40ba95a6cc80cfb41ce49a165d41
martinliporace <martin.liporace@gmail.com>
2022-09-04 12:21:59 +0000

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Task 0"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,7 +9,9 @@ app.url_map.strict_slashes = False
 
 @app.route("/")
 def hello_route():
+    """display “Hello HBNB!"""
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
