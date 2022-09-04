e7473e9f23d8854525013dd1392e5907dbb9ed61
martinliporace <martin.liporace@gmail.com>
2022-09-04 17:47:56 +0000

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Task 3 """
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,16 +9,19 @@ app.url_map.strict_slashes = False
 
 @app.route("/")
 def hello_route():
+    """display Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb")
 def hello_route2():
+    """HBNB"""
     return "HBNB"
 
 
 @app.route("/c/<text>")
 def hello_route3(text):
+    """ display 'C' followed by the value of the text variable"""
     text2 = 'C {}'.format(text.replace('_', ' '))
     return text2
 
@@ -25,6 +29,7 @@ def hello_route3(text):
 @app.route("/python/")
 @app.route("/python/<text>")
 def hello_route4(text='is cool'):
+    """display 'Python', followed by the value of the text variable"""
     text2 = 'Python {}'.format(text.replace('_', ' '))
     return text2
 
