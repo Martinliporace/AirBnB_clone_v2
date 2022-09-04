5e3602fb3cabd4c8fd5434e0ce365c7981bf2253
martinliporace <martin.liporace@gmail.com>
2022-09-04 12:33:18 +0000

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Task 1"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,11 +9,13 @@ app.url_map.strict_slashes = False
 
 @app.route("/")
 def hello_route():
+    """display Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb")
 def hello_route2():
+    """display HBNB"""
     return "HBNB"
 
 if __name__ == '__main__':
